7844ea48e34a7129aad67ad4d8d1474c56b68c8d
Lukas <lukas@leucali.net>
2023-03-05 18:04:45 +0100

    Renamed MQTT Sensors

--- src/mqtt.py
+++ src/mqtt.py
@@ -6,11 +6,12 @@ import paho.mqtt.client as mqtt
 
 class myMQTT():
     def discoverTime(self, device):
-        topic = "homeassistant/sensor/test_sensor/time/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/time/config"
         payload = {
             "device": device,
             "name": "Time",
             "unique_id": "eco2mqtt_time",
+            "object_id": "eco2mqtt_time",
             "unit_of_measurement": "",
             "device_class": "time",
             "state_topic": "ecometer2mqtt/state",
@@ -19,11 +20,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
         
     def discoverTemperature(self, device):
-        topic = "homeassistant/sensor/test_sensor/temp/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/temp/config"
         payload = {
             "device": device,
             "name": "Temperature",
             "unique_id": "eco2mqtt_temp",
+            "object_id": "eco2mqtt_temp",
             "unit_of_measurement": "°C",
             "device_class": "temperature",
             "state_topic": "ecometer2mqtt/state",
@@ -32,11 +34,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
         
     def discoverDistance(self, device):
-        topic = "homeassistant/sensor/test_sensor/distance/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/distance/config"
         payload = {
             "device": device,
             "name": "Distance",
             "unique_id": "eco2mqtt_distance",
+            "object_id": "eco2mqtt_distance",
             "unit_of_measurement": "cm",
             "state_topic": "ecometer2mqtt/state",
             "value_template": "{{ value_json.distance }}",
@@ -44,11 +47,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
         
     def discoverHeight(self, device):
-        topic = "homeassistant/sensor/test_sensor/height/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/height/config"
         payload = {
             "device": device,
             "name": "Height",
             "unique_id": "eco2mqtt_height",
+            "object_id": "eco2mqtt_height",
             "unit_of_measurement": "cm",
             "state_topic": "ecometer2mqtt/state",
             "value_template": "{{ value_json.height }}",
@@ -56,11 +60,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
 
     def discoverLevel(self, device):
-        topic = "homeassistant/sensor/test_sensor/level/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/level/config"
         payload = {
             "device": device,
             "name": "Level",
             "unique_id": "eco2mqtt_level",
+            "object_id": "eco2mqtt_level",
             "unit_of_measurement": "Liter",
             "state_topic": "ecometer2mqtt/state",
             "value_template": "{{ value_json.level }}",
@@ -68,11 +73,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
 
     def discoverCapacity(self, device):
-        topic = "homeassistant/sensor/test_sensor/capacity/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/capacity/config"
         payload = {
             "device": device,
             "name": "Capacity",
             "unique_id": "eco2mqtt_capacity",
+            "object_id": "eco2mqtt_capacity",
             "unit_of_measurement": "Liter",
             "state_topic": "ecometer2mqtt/state",
             "value_template": "{{ value_json.capacity }}",
@@ -80,11 +86,12 @@ class myMQTT():
         self.client.publish(topic, json.dumps(payload))
 
     def discoverPercent(self, device):
-        topic = "homeassistant/sensor/test_sensor/percent/config"
+        topic = "homeassistant/sensor/ecometer2mqtt/percent/config"
         payload = {
             "device": device,
             "name": "Percent",
             "unique_id": "eco2mqtt_percent",
+            "object_id": "eco2mqtt_percent",
             "unit_of_measurement": "%",
             "state_topic": "ecometer2mqtt/state",
             "value_template": "{{ value_json.percent }}",
@@ -112,7 +119,7 @@ class myMQTT():
             "model": "Ecometer S",
             "manufacturer": "Proteus",
             "name": "Ecometer2MQTT",
-            "sw_version": "0.0.1"
+            "sw_version": "0.0.2"
         }
 
         self.discoverTime(device)
